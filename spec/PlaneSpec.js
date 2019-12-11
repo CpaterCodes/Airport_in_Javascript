71011e2e96afff41cea198956fc20008a0ae3dba
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-12-11 15:12:26 +0000

    Tested and implemented checks fagainst plane landing or taking off twice in succession

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,1 +1,18 @@
+describe('Plane',function(){
+  beforeEach(function(){
+    airport = new Airport;
+    plane = new Plane;
+  });
 
+  it('Will not takeoff if already in flight', function(){
+    doubleTakeoff = () => [airport.instructTakeoff(plane)];
+    expect(doubleTakeoff).toThrow('Plane already in flight!');
+  });
+
+  it('Will not land if already landed', function(){
+    airport.instructLand(plane);
+    doubleLand = () => airport.instructLand(plane);
+    expect(doubleLand).toThrow('Plane already landed!');
+  });
+
+});
--- src/Airport.js
+++ src/Airport.js
@@ -3,9 +3,13 @@ function Airport(capacity = 20){
   this.capacity = capacity;
 };
 Airport.prototype.instructLand = function(plane){
-  if ((this.hangar).length === this.capacity)throw "At capacity, turn back!";
+  if((this.hangar).length === this.capacity) throw "At capacity, turn back!";
+  if(plane.inFlight === false) throw "Plane already landed!";
+  plane.land();
   (this.hangar).push(plane);
 };
 Airport.prototype.instructTakeoff = function(plane){
+   if(plane.inFlight === true) throw "Plane already in flight!";
+   plane.takeoff();
   (this.hangar).splice((this.hangar).indexOf(plane),1);
 };
